1a96a09c0e34a5de9a6bb8ce3e3c5a548cfe30e9
SauliusLuk <bendrasinbox-4@yahoo.com>
2022-10-03 19:26:46 +0300

    Initial commit

---
+++ mysite/library/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from .models import (Author, Book, BookInstance, Genre)
+
+# Register your models here.
+class BookInstanceInLine(admin.TabularInline):
+    model = BookInstance
+    readonly_fields = ('uuid', )
+    can_delete = False
+    extra = 0 # isjungia placeholderius
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'isbn', 'author', 'display_genre')
+    inlines = [BookInstanceInLine]
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'book', 'due_back')
+    list_filter = ('status', 'due_back')
+    search_fields = ('uuid', 'book__title', 'book__author__last_name')
+
+    fieldsets = (
+        ('General', {'fields': ('uuid', 'book')}),
+        ('Availability', {'fields': ('status', 'due_back')})
+    )
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'display_books')
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(BookInstance, BookInstanceAdmin)
+admin.site.register(Genre)
---
+++ mysite/library/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(verbose_name="Pavadinimas", max_length=200, help_text="Iveskite knygos zanras")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Žanras"
+        verbose_name_plural = "Žanrai"
+
+class Author(models.Model):
+    first_name = models.CharField("Vardas", max_length=100)
+    last_name = models.CharField("Pavarde", max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def display_books(self):
+        books = self.books.all()
+        book_names = list(book.title for book in books)
+        books_str = ", ".join(book_names)
+        return books_str
+
+    display_books.short_description = 'Knygos'
+
+class Book(models.Model):
+    title = models.CharField("Pavadinimas", max_length=200)
+    summary = models.TextField("Aprasymas", max_length=1000, help_text="Trumpas knygos aprasymas")
+    isbn = models.CharField("ISBN", max_length=13, help_text='13 Simbolių <a href="https://www.isbn-international.org/content/what-isbn">ISBN kodas</a>')
+    author = models.ForeignKey("Author", on_delete=models.SET_NULL, null=True, related_name='books')
+    genre = models.ManyToManyField("Genre", help_text='Pasirinkite knygos zanra')
+
+    def __str__(self):
+        return f"{self.title}, {self.author}"
+
+    def display_genre(self):
+        genres = self.genre.all()
+        genre_names = list(genre.name for genre in genres)
+        genres_str = ", ".join(genre_names)
+        return genres_str
+
+    display_genre.short_description = "Žanras"
+
+
+class BookInstance(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unikalus ID knygos kopijai')
+    book = models.ForeignKey("Book", on_delete=models.CASCADE)
+    due_back = models.DateField("Bus prieinama", blank=True)
+
+    def __str__(self):
+        return f"{self.book}, {self.uuid}"
+
+    class Meta:
+        verbose_name = "Knygos egzempliorius"
+        verbose_name_plural = "Knygos egzemplioriai"
+
+    LOAN_STATUS = (
+        ('a', 'Administruojami'),
+        ('p', 'Paimta'),
+        ('g', 'Galima paimti'),
+        ('r', 'Rezervuota'),
+
+    )
+
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='a', help_text='Statusas')
+
