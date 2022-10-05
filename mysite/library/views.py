b25022482ee61746387a6f5ec42d56bcb8338269
SauliusLuk <bendrasinbox-4@yahoo.com>
2022-10-05 14:45:23 +0300

    commit

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Book, BookInstance, Author
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Labas, pasauli!')
+    # Suskaičiuokime keletą pagrindinių objektų
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Laisvos knygos (tos, kurios turi statusą 'g')
+    num_instances_available = BookInstance.objects.filter(status__exact='g').count()
+
+    # Kiek yra autorių
+    num_authors = Author.objects.all().count()
+
+    # Perduoti informaciją į šabloną žodyno pavidale:
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'index.html', context=context)
