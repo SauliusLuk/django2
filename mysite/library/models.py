9126ff4c7f2872ffd2218196d33096d86c8ce4f6
Saulius Lukosius <bendrasinbox-4@yahoo.com>
2022-10-13 13:29:57 +0300

    10. Pridetas profilio nuotraukos automatinis sumazinimas

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -3,6 +3,7 @@ import uuid
 from django.contrib.auth.models import User
 from datetime import date
 from tinymce.models import HTMLField
+from PIL import Image
 
 
 # Create your models here.
@@ -107,3 +108,10 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} profilis"
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        super().save(force_insert, force_update, using, update_fields)
+        img = Image.open(self.nuotrauka.path)
+        if img.height > 300 or img.width > 300:
+            img.thumbnail((200, 200))
+            img.save(self.nuotrauka.path)
