8a5a478bf8e450efa01239990fd00d5f36a10f33
Saulius Lukosius <bendrasinbox-4@yahoo.com>
2022-10-16 17:59:01 +0300

    12. views.py vertimas

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -4,29 +4,25 @@ from django.contrib.auth.models import User
 from datetime import date
 from tinymce.models import HTMLField
 from PIL import Image
-
-
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
+
 class Genre(models.Model):
-    name = models.CharField(verbose_name="Pavadinimas", max_length=200, help_text="Iveskite knygos zanra")
+    name = models.CharField("Pavadinimas", max_length=200, help_text="Įveskite knygos žanrą (pvz. detektyvas)")
 
     def __str__(self):
         return f"{self.name}"
 
     class Meta:
-        verbose_name = "Žanras"
-        verbose_name_plural = "Žanrai"
+        verbose_name = _('Genre')
+        verbose_name_plural = _('Genres')
 
 
 class Author(models.Model):
     first_name = models.CharField("Vardas", max_length=100)
-    last_name = models.CharField("Pavarde", max_length=100)
-    # description = models.TextField('Aprašymas', max_length=2000, default='')
-    description = HTMLField('Aprašymas')
-    class Meta:
-        verbose_name = "Autorius"
-        verbose_name_plural = "Autoriai"
-        ordering = ['-id']
+    last_name = models.CharField("Pavardė", max_length=100)
+    # description = models.TextField("Aprašymas", max_length=2000, default="")
+    description = HTMLField("Aprašymas")
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
@@ -37,25 +33,24 @@ class Author(models.Model):
         books_str = ", ".join(book_names)
         return books_str
 
-    display_books.short_description = 'Knygos'
+    display_books.short_description = "Knygos"
+
+    class Meta:
+        verbose_name = _('Author')
+        verbose_name_plural = _('Authors')
+        ordering = ['id']
 
 
 class Book(models.Model):
-    title = models.CharField("Pavadinimas", max_length=200)
-    summary = models.TextField("Aprašymas", max_length=1000, help_text="Trumpas knygos aprašymas")
-    isbn = models.CharField("ISBN", max_length=13,
-                            help_text='13 Simbolių <a href="https://www.isbn-international.org/content/what-isbn">ISBN kodas</a>')
-    author = models.ForeignKey("Author", on_delete=models.SET_NULL, null=True, related_name='books')
-    genre = models.ManyToManyField("Genre", help_text='Pasirinkite knygos zanra')
-    cover = models.ImageField('Viršelis', upload_to='covers', null=True)
-
-    class Meta:
-        verbose_name = "Knyga"
-        verbose_name_plural = "Knygos"
-        ordering = ['id']
+    title = models.CharField(_("Title"), max_length=200)
+    summary = models.TextField(_("Summary"), max_length=1000, help_text='Trumpas knygos aprašymas')
+    isbn = models.CharField("ISBN", max_length=13, help_text='13 Simbolių <a href="https://www.isbn-international.org/content/what-isbn">ISBN kodas</a>')
+    author = models.ForeignKey("Author", on_delete=models.SET_NULL, null=True, related_name="books")
+    genre = models.ManyToManyField("Genre", help_text='Išrinkite žanrą(us) šiai knygai')
+    cover = models.ImageField(_("Cover"), upload_to='covers', null=True)
 
     def __str__(self):
-        return f"{self.title}, {self.author}"
+        return f"{self.author} - {self.title}"
 
     def display_genre(self):
         genres = self.genre.all()
@@ -65,6 +60,10 @@ class Book(models.Model):
 
     display_genre.short_description = "Žanras"
 
+    class Meta:
+        verbose_name = _('Book')
+        verbose_name_plural = _('Books')
+        ordering = ['-id']
 
 class BookInstance(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, help_text='Unikalus UUID knygos kopijai')
@@ -72,15 +71,9 @@ class BookInstance(models.Model):
     due_back = models.DateField("Bus prieinama", blank=True)
     reader = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
-    LOAN_STATUS = (
-        ('a', 'Administruojami'),
-        ('p', 'Paimta'),
-        ('g', 'Galima paimti'),
-        ('r', 'Rezervuota'),
+    LOAN_STATUS = (('a', 'Administruojama'), ('p', 'Paimta'), ('g', 'Galima paimti'), ('r', 'Rezervuota'))
 
-    )
-
-    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='a', help_text='Statusas')
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='a', help_text="Statusas")
 
     def is_overdue(self):
         if self.due_back and date.today() > self.due_back:
@@ -88,23 +81,24 @@ class BookInstance(models.Model):
         return False
 
     def __str__(self):
-        return f"{self.book}, {self.uuid}"
+        return f"{self.book} ({self.uuid}) - {self.status} ({self.due_back})"
 
     class Meta:
-        verbose_name = "Knygos egzempliorius"
-        verbose_name_plural = "Knygos egzemplioriai"
+        verbose_name = _('Book Instance')
+        verbose_name_plural = _('Book Instances')
         ordering = ['-due_back']
 
+
 class BookReview(models.Model):
-    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True, blank=True, related_name='reviews')
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True, blank=True)
     reviewer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    date_created = models.DateTimeField(auto_now_add=True)
-    content = models.TextField('Atsiliepimas', max_length=2000)
+    date_created = models.DateTimeField("Data", auto_now_add=True)
+    content = models.TextField("Atsiliepimas", max_length=2000)
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    nuotrauka = models.ImageField(default="default.png", upload_to="profile_pics")
+    nuotrauka = models.ImageField(default='default.png', upload_to="profile_pics")
 
     def __str__(self):
         return f"{self.user.username} profilis"
@@ -113,5 +107,6 @@ class Profile(models.Model):
         super().save(force_insert, force_update, using, update_fields)
         img = Image.open(self.nuotrauka.path)
         if img.height > 300 or img.width > 300:
-            img.thumbnail((200, 200))
+            img.thumbnail((300, 300))
             img.save(self.nuotrauka.path)
+
--- mysite/library/views.py
+++ mysite/library/views.py
@@ -10,7 +10,7 @@ from django.views import generic
 from django.views.decorators.csrf import csrf_protect
 from django.views.generic import ListView, UpdateView, DeleteView, DetailView, CreateView
 from django.views.generic.edit import FormMixin
-
+from django.utils.translation import gettext as _
 
 
 # Create your views here.
@@ -27,20 +27,24 @@ def register(request):
         if password == password2:
             # tikriname, ar neužimtas username
             if User.objects.filter(username=username).exists():
-                messages.error(request, f'Vartotojo vardas {username} užimtas!')
+                messages.error(request, _('Username %s already exists!') % username)
+                # messages.error(request, f"Vartotojo vardas {username} užimtas!")
                 return redirect('register')
             else:
                 # tikriname, ar nėra tokio pat email
                 if User.objects.filter(email=email).exists():
-                    messages.error(request, f'Vartotojas su el. paštu {email} jau užregistruotas!')
+                    messages.error(request, _('Email %s already exists!') % email)
+                    # messages.error(request, f'Vartotojas su el. pašto adresu {email} jau užregistruotas!')
                     return redirect('register')
                 else:
                     # jeigu viskas tvarkoje, sukuriame naują vartotoją
                     User.objects.create_user(username=username, email=email, password=password)
-                    messages.info(request, f'Vartotojas {username} užregistruotas!')
+                    messages.info(request, _('New user %s registered!') % username)
+                    # messages.info(request, f'Naujas vartotojas {username} registruotas!')
                     return redirect('login')
         else:
-            messages.error(request, 'Slaptažodžiai nesutampa!')
+            # messages.error(request, 'Slaptažodžiai nesutampa')
+            messages.error(request, _('Passwords do not match!'))
             return redirect('register')
     return render(request, 'registration/register.html')
 
